4922f8c60ee8734bfe7d1f198fd81136d755cee1
successJohn <successojohn@gmail.com>
2021-12-07 15:01:48 -0800

    Submission of Form from request body

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require("express");
-
 const path = require("path");
-
-
 const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const UserForm = require("./models/userform");
+app.use(express.static("Views"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
+mongoose.connect("mongodb://localhost/myBBQDB", {useNewUrlParser: true})
 
-app.use(express.static("public"))
+
+
 
 
 app.get("/", (req,res) =>{
     //res.send("Hello World");
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "./Views/index.html"));
 });
+
+app.post("/posts/userform",(req,res)=>{
+    console.log(req.body);
+    res.redirect("/");
+})
 
 app.listen(3000, ()=>{
     console.log("app listening ")
